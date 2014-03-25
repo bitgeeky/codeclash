3531f9726003cf0f93e64f5f23ad4be6b296955d
bitgeeky <mozpankaj1994@gmail.com>
2014-03-26 00:08:05 +0530

    Board at Server and Client Side

---
+++ public/js/Board.js
@@ -0,0 +1,29 @@
+/*****************************
+** GAME BOARD
+*****************************/
+var Board = function(rows, columns){
+    
+    var cols = columns;
+    var rows = rows;
+    var tiles = Create2DArray(rows, cols);
+
+    function Create2DArray(r,c) {
+        var arr = Array(r);
+        for (var i=0;i<r;i++) {
+            arr[i] = Array(c);
+        }
+        return arr;
+    }
+
+    var update = function(arr){
+        for(var i=0;i<rows;i++){
+            for(var j=0;j<cols;j++){
+               tiles[i][j] = arr[i][j]; 
+            }
+        }
+    };
+
+    return{
+        update: update
+    }
+};
