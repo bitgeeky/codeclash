2bacc01d7b39b1af629dc569a9e0d27c0a17a41e
bitgeeky <mozpankaj1994@gmail.com>
2014-03-27 18:39:42 +0530

    Structure for Player AI

---
+++ redPlayer.js
@@ -0,0 +1,10 @@
+var redPlayer = function(){
+    var makeMove = function(){
+
+        // Write the bot AI here 
+    };
+    return{
+        makeMove: makeMove
+    }
+};
+exports.redPlayer = redPlayer;
