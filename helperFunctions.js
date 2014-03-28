92e7efd65915b24d1ec62e75ad68791f560f88a7
bitgeeky <mozpankaj1994@gmail.com>
2014-03-28 22:26:17 +0530

    makeMove Added

--- helperFunctions.js
+++ helperFunctions.js
@@ -1,10 +1,88 @@
 var helperFunctions = function(){
+    var stepRange = 1;
+    var fireRange = 5;
+    
+
     var validateMove = function(move, color){
     
     };
+
+
     var makeMove = function(tiles, move){
+        
         // Return a 2d array updating the board state
+        
+        var initX = move[0],
+            initY = move[1];
+
+            // Make a Valid Move
+            if(move[3] == 1)
+            {
+                if(move[4] == 0){
+                    // Move towards North
+                    tiles[initX][initY+stepRange] = tiles[initX][initY];
+                    tiles[initX][initY] = "GR";
+                }
+                else{
+                    // Fire towards North
+                    for(var i=0;i<fireRange;i++){
+                        if((initX - i) >= 0){
+                            tiles[initX-i][initY] = "FR";
+                        }
+                    }
+                }
+            }
+            else if(move[3] == -1)
+            {
+                if(move[4] == 0){
+                    // Move towards South
+                    tiles[initX][initY-stepRange] = tiles[initX][initY];
+                    tiles[initX][initY] = "GR";
+                }
+                else{
+                    // Fire towards South
+                    for(var i=0;i<fireRange;i++){
+                        if((initX + i) <= tiles.length){
+                            tiles[initX+i][initY] = "FR";
+                        }
+                    }
+                }
+            }
+            else if(move[3] == 2)
+            {
+                if(move[4] == 0){
+                    // Move towards East
+                    tiles[initX+stepRange][initY] = tiles[initX][initY];
+                    tiles[initX][initY] = "GR";
+                }
+                else{
+                    // Fire towards East
+                    for(var i=0;i<fireRange;i++){
+                        if((initY + i) <= tiles[0].length){
+                            tiles[initX][initY+i] = "FR";
+                        }
+                    }
+                }
+            }
+            else if(move[3] == -2)
+            {
+                if(move[4] == 0){
+                    // Move towards West
+                    tiles[initX-stepRange][initY] = tiles[initX][initY];
+                    tiles[initX][initY] = "GR";
+                }
+                else{
+                    // Fire towards West
+                    for(var i=0;i<fireRange;i++){
+                        if((initY - i) >= 0){
+                            tiles[initX][initY-i] = "FR";
+                        }
+                    }
+                }
+            }
+        return tiles;
     };
+
     return{
         validateMove: validateMove,
         makeMove: makeMove
--- redPlayer.js
+++ redPlayer.js
@@ -2,6 +2,13 @@ var redPlayer = function(){
     var makeMove = function(){
 
         // Write the bot AI here 
+        // Return an array havin 4 values
+        // 1 Initial co-ordinates of Tank
+        // 2 Direction of firing/moving
+        // 1:North -1:South
+        // 2:East  -2:West
+        // Range of firing is 5 units
+        // 3 Bool 0-move and 1-fire
     };
     return{
         makeMove: makeMove
