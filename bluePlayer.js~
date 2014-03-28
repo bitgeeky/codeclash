3db77c2573c8770222f1112b1ec9cdef86f91a37
bitgeeky <mozpankaj1994@gmail.com>
2014-03-29 03:10:34 +0530

    Random moves for both Players

---
+++ bluePlayer.js~
@@ -0,0 +1,10 @@
+var bluePlayer = function(){
+    var makeMove = function(){
+
+        // Write the bot AI here 
+    };
+    return{
+        makeMove: makeMove
+    }
+};
+exports.bluePlayer = bluePlayer;
--- redPlayer.js
+++ redPlayer.js
@@ -1,5 +1,5 @@
 var redPlayer = function(){
-    var makeMove = function(){
+    var makeMove = function(tiles, playerTank){
 
         // Write the bot AI here 
         // Return an array havin 4 values
@@ -9,6 +9,18 @@ var redPlayer = function(){
         // 2:East  -2:West
         // Range of firing is 5 units
         // 3 Bool 0-move and 1-fire
+
+        for(var i=0;i<=tiles.length;i++){
+            for(var j=0;j<=tiles[0].length;j++){
+                if(tiles[i][j] == playerTank)
+                    break;
+            }
+        }
+        var dirs = [1,-1,2,-2];
+        var dir = Math.floor((Math.random()*4)+1) -1;
+        var fire = Math.floor((Math.random()*2)+1) -1;
+        var move = [i, j, dirs[dir], fire];
+        return move;
     };
     return{
         makeMove: makeMove
