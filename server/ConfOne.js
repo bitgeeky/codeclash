f49e96a5721c021edae834e8d7dba78635b807f3
bitgeeky <mozpankaj1994@gmail.com>
2014-04-04 23:39:56 +0530

    Added License

--- server/ConfOne.js
+++ server/ConfOne.js
@@ -1,3 +1,36 @@
+/*
+ * @overview AI Moderating Bot built using HTML5 Canvas, Node.js and Socket.IO
+ * @version 0.0.1
+ * @source https://github.com/bitgeeky/moderator
+ *
+ * @author Pankaj Malhotra(:bitgeeky)
+ * @email mozpankaj1994@gmail.com
+ */
+
+/*
+ * Copyright (c) 2014 Pankaj Malhotra
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ *    1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ *
+ *    2. Altered source versions must be plainly marked as such, and must not
+ *    be misrepresented as being the original software.
+ *
+ *    3. This notice may not be removed or altered from any source
+ *    distribution.
+ */
+
+
 var ConfOne = function(r, c){
 
     var numPlayers = 5;
